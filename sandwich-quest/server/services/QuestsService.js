5745a88c06620b8f7204fa9706cf4de5a8a4ce3d
Dominic Rupert <dominicrupert@gmail.com>
2022-06-29 14:48:48 -0600

    more

--- sandwich-quest/server/services/QuestsService.js
+++ sandwich-quest/server/services/QuestsService.js
@@ -4,10 +4,41 @@ import {BadRequest} from "@bcwdev/auth0provider/lib/Errors.js";
 class QuestsService{
     async getAllQuests(query={}){
         let quests = await dbContext.Quests.find(query).populate(
-            
+            "creator",
+            "name picture"
+        );
+        return quests;
 
-        )
+        }
+    async getQuestById(id){
+        let quest = await dbContext.Quests.findById(id).populate(
+            "creator",
+            "name picture"
+        );
+        return quest;
+    }
+    async createQuest(quest){
+        let newQuest = await dbContext.Quests.create(quest);
+        await newQuest.populate("creator", "name picture")
+        return newQuest;
+    }
+
+
+    // TODO: Add updateQuest
+
+    async deleteQuest(id, userId){
+        let quest = await dbContext.Quests.findById(id);
+        if(!quest){
+            throw new BadRequest("Invalid quest id");
+        }
+        if(quest.creatorId.toString() !== userId){
+            throw new BadRequest("You do not have permission to delete this quest");
+        }
+
+        await quest.remove();
+    }
+
 }
 
-}
+
 export const questsService = new QuestsService();
