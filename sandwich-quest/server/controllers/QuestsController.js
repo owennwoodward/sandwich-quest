77cfebf7583c61105740a1e4071d4f57b35cef9c
Owen Woodward <owenwoodward555@gmail.com>
2022-06-29 14:50:39 -0600

    sending controller

---
+++ sandwich-quest/server/controllers/QuestsController.js
@@ -0,0 +1,41 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
+import BaseController from "../utils/BaseController";
+
+
+
+export class QuestsController extends BaseController {
+    constructor() {
+        super('api/quests')
+        this.router
+            .get('', this.getAllQuests)
+            .use(Auth0Provider.getAuthorizedUserInfo)
+            .post('', this.createQuest)
+            .delete('/:id', this.removeQuest)
+    }
+
+    async getAllQuests(req, res, next) {
+        try {
+            const quests = await questsService.getAllQuests()
+            return res.send(quests)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async createQuest(req, res, next) {
+        try {
+            const quest = await questsService.createQuest(req.body)
+            return res.send(quest)
+        } catch (error) {
+            next(error)
+        }
+    }
+    async removeQuest(req, res, next) {
+        try {
+            const message = await questsService.removeQuest(req.params.id, req.userInfo.id)
+            return res.send('deleted', message)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
--- sandwich-quest/server/db/DbContext.js
+++ sandwich-quest/server/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
+import { QuestSchema } from '../models/Quest';
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  Quest = mongoose.model('Quest', QuestSchema)
 }
 
 export const dbContext = new DbContext()
