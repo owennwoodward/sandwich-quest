2641e53469db0d9564c280678e89bf983c900899
Dominic Rupert <dominicrupert@gmail.com>
2022-06-29 15:28:47 -0600

    item service

--- sandwich-quest/server/services/QuestItemsService.js
+++ sandwich-quest/server/services/QuestItemsService.js
@@ -4,7 +4,35 @@ import { BadRequest } from "@bcwdev/auth0provider/lib/Errors.js";
 
 class QuestItemService{
     async getQuestItems(accountId){
-        
+        let questItems = await dbContext.QuestItem.find({accountId})
+        .populate("creator", "name picture")
+        return questItems;
+    }
+
+    async createQuestItem(questItem){
+        let newQuestItem = await dbContext.QuestItem.create(questItem);
+        await newQuestItem.populate("creator")
+      
+        return newQuestItem;
+    }
+
+    async removeQuestItem(id, userId){
+        let questItem = await dbContext.QuestItem.findById(id);
+        if(!questItem){
+            throw new BadRequest("Invalid quest item id");
+        }
+        if(questItem.creatorId.toString() !== userId){
+            throw new BadRequest("You do not have permission to delete this quest item");
+        }
+
+        await questItem.remove();
+    }
+
+
+  
+
 
 
 }
+
+export const questItemService = new QuestItemService();
