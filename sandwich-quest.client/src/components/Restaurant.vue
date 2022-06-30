ae08d2537fb04a5b5b397fde6b1b376cec2649fe
Dominic Rupert <dominicrupert@gmail.com>
2022-06-30 11:02:21 -0600

    details

---
+++ sandwich-quest.client/src/components/Restaurant.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>{{ restaurant.name }}</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="{{restaurant.image_url}}" class="img-fluid" alt="Responsive image">
+                            </div>
+
+
+                        </div>
+
+
+                    </div>
+</template>
+
+
+<script>
+import {useRouter} from "vue-router"
+export default {
+    props: {
+        restaurant: {
+            type: Object,
+            required: true
+        }
+    },
+    setup(props) {
+        const router = useRouter()
+        return {
+            restaurant: props.restaurant,
+            goToRestaurantDetails: () => {
+                router.push({
+                    name: "restaurant-details",
+                    params: {
+                        id: props.restaurant.id
+                    }
+                })
+            }
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
