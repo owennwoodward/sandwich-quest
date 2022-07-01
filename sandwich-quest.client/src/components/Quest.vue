e041921a287eafcfe789106d8d150886d195d908
TaylorBruun <taylorbruun@gmail.com>
2022-07-01 15:37:31 -0600

    quest items

--- sandwich-quest.client/src/components/Quest.vue
+++ sandwich-quest.client/src/components/Quest.vue
@@ -3,7 +3,7 @@
         <div class="row card p-3 m-3 bg-primary selectable">
             <div class="col-12 text-center" @click="">
                 <h5>{{ quest.name }}</h5>
-
+                <QuestItem v-for="i in questItems" :key="i.id" :item="i" />
             </div>
 
         </div>
@@ -18,9 +18,9 @@ import { AppState } from '../AppState'
 
 export default {
     props: {quest : {type: Object, required: true}},
-    setup(){
+    setup(props){
         return {
-            questItems: computed(()=> AppState.questitems)
+            questItems: computed(()=> AppState.questitems.filter(i => i.questId == props.quest.id))
         }
     }
 }
--- sandwich-quest.client/src/components/QuestItem.vue
+++ sandwich-quest.client/src/components/QuestItem.vue
@@ -1,98 +1,31 @@
-<template lang="">
+<template>
+  <div class="item-card elevation-2 py-2 my-2">
+      <h6>{{item.name}}</h6>
+      {{item.myNotes}}
 
-
-<!--  -->
-    <!-- <div class="container text-black py-3">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3>{{ homeRestaurant.name }}<small class="text-muted"> -  {{homeRestaurant.price}}</small></h3>
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <img :src="homeRestaurant.image_url" class="img-fluid" alt="Responsive image">
-                            </div>
-                                <div class="col-md-8 d-flex justify-content-between p-2">
-                                    <div class="" >
-                                        <h4>{{ homeRestaurant.display_phone }}</h4>
-                                        <div> <YelpStars class="stars" :rating="homeRestaurant.rating"/> </div>
-                                    </div>
-
-                                    <div>
-                                        <AddToQuest :restaurantId="routeId" :quests="quests"/>
-                                    </div>
-
-                                </div>
-
-                                <div>
-                                    <p>{{homeRestaurant.location?.address1}}
-                                    {{homeRestaurant.location?.address2}}
-                                    {{homeRestaurant.location?.address3}}
-                                    {{homeRestaurant.location?.city}}
-                                    {{homeRestaurant.location?.state}}</p>
-
-                                    <P v-for="c in homeRestaurant.categories" :key="c.alias"> {{c.title}}</P>
-                                </div>
-
-                                        
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <h4>{{ homeRestaurant.review_count }}</h4>
-                                    </div>
-                                </div>
-                             
-            -->
-                                <!-- </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div> -->
-            <!-- </div> -->
-
-            <!-- TODO Beware, ye who enter here -->
-
-
-<!--  -->
-
-
-  <div class="card text-dark p-3 my-4 selectable">
-    <HomeRestaurant :homeRestaurant="itemInfo"/>
   </div>
 </template>
+
+
 <script>
-import { onMounted, computed } from "vue"
-import { yelpService } from "../services/YelpService"
-import Pop from "../utils/Pop"
-
 export default {
-  props: {
-    quest: {
-      type: Object, required: true
-    }
-  },
-  setup(props) {
-
-    const restaurantInfo = {}
-
-    onMounted(async () => {
-      try {
-        restaurantInfo = await yelpService.getById(props.quest.id)
-      } catch (error) {
-        Pop.toast(error, 'error')
-        console.error(error)
-      }
-    })
-
+  props : {item: {type: Object, required: true}},
+  setup(props){
     return {
-      itemInfo: computed(()=> restaurantInfo)
+
     }
   }
 }
 </script>
-<style scoped lang="scss">
-  
+
+
+<style lang="scss" scoped>
+@import "../assets/scss/variables";
+
+.item-card{
+  background-color: $dark;
+  color: $light
+}
+
+
 </style>
