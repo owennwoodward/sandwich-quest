6244188c8a62aba8cda963e045744e9a1727f181
Cameron <cameron.kueneman@gmail.com>
2022-07-01 09:27:37 -0600

    morning

--- sandwich-quest.client/src/pages/RestaurantDetailsPage.vue
+++ sandwich-quest.client/src/pages/RestaurantDetailsPage.vue
@@ -11,16 +11,23 @@
                             <div class="col-md-4">
                                 <img :src="homeRestaurant.image_url" class="img-fluid" alt="Responsive image">
                             </div>
-                                <div class="col-md-8">
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <h4>{{ homeRestaurant.display_phone }}</h4>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
+                                <div class="col-md-8 d-flex justify-content-between p-2">
+                                    <div class="" >
+                                        <h4>{{ homeRestaurant.display_phone }}</h4>
                                         <h4>Rating: {{ homeRestaurant.rating }}/5</h4>
                                     </div>
+                                    <div>
+                                        <div class="btn btn-primary " @click="">Add to Collection</div>
+                                        <form>
+                                            <select>
+                                                <option>Colection 1 Fix me</option>
+                                                <option>Colection 2</option>
+                                                <option>Colection 3</option>
+                                                <option>Colection 4</option>
+                                            </select>
+                                        </form>
+                                    </div>
+                                </div>
                                         <!-- 
                                 </div>
                                 <div class="row">
@@ -31,13 +38,12 @@
                              
             -->
                                 </div>
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             </div>
-            </div>
+            <!-- </div> -->
 
 </template>
 
@@ -46,6 +52,7 @@
 import { computed, onMounted } from '@vue/runtime-core'
 import { useRoute } from "vue-router"
 import { AppState } from "../AppState.js"
+import { accountService } from "../services/AccountService.js"
 import { yelpService } from "../services/YelpService.js"
 import { logger } from "../utils/Logger.js"
 export default {
@@ -55,13 +62,17 @@ export default {
         onMounted(() => {
             try {
               yelpService.getById(route.params.id)
+              accountService.getQuests()
             } catch (error) {
               logger.error(error)
               Pop.toast(error.message, 'error')
             }
         })
         return {
-            homeRestaurant:computed(()=> AppState.activeRestaurant)
+            homeRestaurant:computed(()=> AppState.activeRestaurant),
+            addToCollection() {
+
+            }
 
         }
     }
