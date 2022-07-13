f84409803c5b04ce55375ec685aa76c31f8f350c
TaylorBruun <taylorbruun@gmail.com>
2022-07-13 15:10:36 -0600

    location is asked for and used

--- sandwich-quest.client/src/services/YelpService.js
+++ sandwich-quest.client/src/services/YelpService.js
@@ -3,20 +3,25 @@ import { api } from './AxiosService'
 import { baseURL } from '../env'
 import { AppState } from '../AppState'
 import { logger } from '../utils/Logger.js'
+import { locationService } from './LocationService'
 export const yelp = Axios.create({
   baseURL,
   timeout: 8000,
   })
 
 
-let location = 'boise'
+let location = ''
 let categories = ''
 let term = ''
+let coords = {}
 class YelpService {
     async getAll(query = '') {
+        await locationService.getUserCoordinates()
+        location = AppState.currentLocation || 'boise'
         categories = AppState.currentCategories
+        coords = AppState.currentCoords.coords
         term = query
-        const res = await api.get('yelp', {params: {categories: categories, location: location, term: term }})
+        const res = await api.get('yelp', {params: {categories: categories, location: location, latitude: coords.latitude, longitude:coords.longitude, term: term }})
         // console.log(res.data, 'here is the get all res')
         AppState.homeRestaurants = res.data
     }
