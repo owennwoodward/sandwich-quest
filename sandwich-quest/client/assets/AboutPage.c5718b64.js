56b89ae1990f3ba330e104891bc960ff53ee025b
Owen Woodward <owenwoodward555@gmail.com>
2022-07-15 17:59:38 -0600

    build 3.1.10

---
+++ sandwich-quest/client/assets/AboutPage.c5718b64.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.903494ec.js";import{o,b as t,d as s}from"./vendor.92888ce4.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),r=[n];function _(d,i,p,u,f,h){return o(),t("div",c,r)}var b=e(a,[["render",_]]);export{b as default};
