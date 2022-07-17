1fec5a7450bfbce9678cd1392e24ab6a9a51be9e
TylerRice27 <Tylerrayrice2@gmail.com>
2022-07-17 15:52:26 -0600

    Took out About Page and commented Pop Toast

--- sandwich-quest.client/src/pages/AccountPage.vue
+++ sandwich-quest.client/src/pages/AccountPage.vue
@@ -1,29 +1,47 @@
 <template>
   <div v-if="account.id" class="container">
     <div class="d-flex justify-content-end pt-2">
-      <button class="  btn btn-danger " @click="logout">Logout <i class="mdi mdi-logout">
-        </i></button>
+      <button class="btn btn-danger" @click="logout">
+        Logout <i class="mdi mdi-logout"> </i>
+      </button>
     </div>
-
 
     <div class="about text-center">
       <img class="rounded-pill p-3" :src="account.picture" alt="" />
       <div class="text-break">
-      <h1>{{ account.name }}</h1>
-</div>
+        <h1>{{ account.name }}</h1>
+      </div>
 
-      <button class="btn btn-success" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop"
-        aria-controls="offcanvasTop">Edit Account</button>
+      <button
+        class="btn btn-success"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasTop"
+        aria-controls="offcanvasTop"
+      >
+        Edit Account
+      </button>
 
-      <div class="offcanvas offcanvas-top  bg-dark" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+      <div
+        class="offcanvas offcanvas-top bg-dark"
+        tabindex="-1"
+        id="offcanvasTop"
+        aria-labelledby="offcanvasTopLabel"
+      >
         <div class="offcanvas-header">
           <h5 id="offcanvasTopLabel">Edit Account</h5>
-          <i class=" mdi mdi-window-close btn-dark bg-dark text-reset selectable" data-bs-dismiss="offcanvas"
-            aria-label="Close"></i>
+          <i
+            class="mdi mdi-window-close btn-dark bg-dark text-reset selectable"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></i>
         </div>
         <div class="offcanvas-body">
-
-          <form class="d-flex flex-column align-items-center" action="" @submit.prevent="editAccount">
+          <form
+            class="d-flex flex-column align-items-center"
+            action=""
+            @submit.prevent="editAccount"
+          >
             <div class="col-md-6 d-flex flex-column">
               <div class="p-2 d-flex justify-content-around">
                 <label class="ms-5" for="">Account name:</label>
@@ -34,19 +52,20 @@
                 <input class="me-5" type="text" v-model="edits.picture" />
               </div>
             </div>
-            <button data-bs-dismiss="offcanvas" aria-label="Save" class="btn btn-primary mt-3">Save</button>
+            <button
+              data-bs-dismiss="offcanvas"
+              aria-label="Save"
+              class="btn btn-primary mt-3"
+            >
+              Save
+            </button>
           </form>
         </div>
       </div>
 
-
-
-
       <h3 class="mt-4 py-2">My Quests:</h3>
       <Quest v-for="q in quests" :key="q.id" :quest="q" />
-
     </div>
-
   </div>
 
   <Login v-if="!account.id" />
@@ -80,7 +99,7 @@ export default {
         await locationService.getUserCoordinates()
       } catch (error) {
         console.error(error)
-        // Pop.toast(error, 'error')
+
       }
     })
     return {
